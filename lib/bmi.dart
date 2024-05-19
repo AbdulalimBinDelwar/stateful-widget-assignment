1f641679037a3cea6380bf29f90319eb437e8d2e
Abdul Alim <abdalim5421@gmail.com>
2024-05-19 19:41:27 +0600

    commit

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class BmiCalculator extends StatefulWidget {
   const BmiCalculator({super.key});
@@ -70,6 +71,45 @@ class _BmiCalculatorState extends State<BmiCalculator> {
                 IconButton(onPressed: (){}, icon: Icon(Icons.chevron_right,size: 40,))
               ],
             ),
+        SfRadialGauge(
+            title: GaugeTitle(
+                text: 'Speedometer',
+                textStyle:
+                const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+            axes: <RadialAxis>[
+              RadialAxis(minimum: 15, maximum: 30, ranges: <GaugeRange>[
+                GaugeRange(
+                    startValue: 15,
+                    endValue: 18,
+                    color: Colors.red,
+                    startWidth: 10,
+                    endWidth: 10),
+                GaugeRange(
+                    startValue: 18.5,
+                    endValue: 24,
+                    color: Colors.green,
+                    startWidth: 10,
+                    endWidth: 10),
+                GaugeRange(
+                    startValue: 24.5,
+                    endValue: 40,
+                    color: Colors.red,
+                    startWidth: 10,
+                    endWidth: 10)
+              ], pointers: <GaugePointer>[
+                NeedlePointer(value: 90)
+              ], annotations: <GaugeAnnotation>[
+                GaugeAnnotation(
+                    widget: Container(
+                        child: const Text('50.0',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold))),
+                    angle: 90,
+                    positionFactor: 0.5)
+              ])
+            ]
+        ),
+
 
           ],
         ),
